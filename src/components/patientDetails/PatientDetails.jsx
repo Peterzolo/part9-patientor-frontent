2bbc1f1fc68d198cc0ac222e24aeaae641b8de1f
Peter <petersolomon704@gmail.com>
2023-06-01 18:13:46 +0100

    part 9
    modified code

--- src/components/patientDetails/PatientDetails.jsx
+++ src/components/patientDetails/PatientDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getPatient } from "../../services/patients";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const PatientDetails = () => {
   const { id } = useParams();
@@ -22,10 +23,50 @@ const PatientDetails = () => {
   }, [id]);
 
   return (
-    <div>
-      <h2>Detail</h2>
-    </div>
+    <Wrapper>
+      <h2 className="patient-detail-title">Patient's Data</h2>
+      <hr className="horizontal-rule" />
+      <div className="item-wrap">
+        <div className="item-title">Name</div>
+        <div className="item">{patient.name}</div>
+      </div>
+      <div className="item-wrap">
+        <div className="item-title">Occupation</div>
+        <div className="item">{patient.occupation}</div>
+      </div>
+      <div className="item-wrap">
+        <div className="item-title">Date of birth</div>
+        <div className="item">{patient.dateOfBirth}</div>
+      </div>
+      <div className="item-wrap">
+        <div className="item-title">Gender</div>
+        <div className="item">{patient.gender}</div>
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: lightgray;
+  border-radius: 10px;
+  .patient-detail-title {
+    text-align: center;
+    color: blue;
+  }
+  .horizontal-rule {
+    height: 10px;
+    background-color: #efeded;
+    border: none;
+  }
+  .item-wrap {
+    width: 80%;
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+  }
+`;
 
 export default PatientDetails;
